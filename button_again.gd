d1b49c5e852c690e923e183c0e86dfe50d9ee2f3
Kamila Ignatowicz <2ignatow@informatik.uni-hamburg.de>
2018-01-27 14:17:45 +0100

    Gifs umschalten

--- button_again.gd
+++ button_again.gd
@@ -4,8 +4,14 @@ extends Button
 # var a = 2
 # var b = "textvar"
 func _on_button_pressed():
-    var teleshopping = get_node("/root/World/tv/Teleshopping1")
-    teleshopping.queue_free()
+    var teleshopping1 = get_node("/root/World/tv/Teleshopping1")
+    var teleshopping2 = get_node("/root/World/tv/Teleshopping2")
+    if(teleshopping1.is_visible()):
+        teleshopping1.hide()
+        teleshopping2.show()
+    else:
+        teleshopping2.hide()
+        teleshopping1.show()
     print("removed gif 1")
 
 func _ready():
